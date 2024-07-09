169b62f6e0ff0317d54bcc55302215d58eb3d639
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-07-09 19:03:42 +0100

    refactor: refactor List class component to functional component

--- src/list/List.tsx
+++ src/list/List.tsx
@@ -3,39 +3,30 @@ import { Person } from "../models/Person";
 import "./List.css"
 
 interface Props {
-    result: Person[]
+    result: Person[];
 }
 
-class List extends React.Component<Props> {
-    constructor(props: Props) {
-        super(props);
-    }
-
-    render() {
-        const data = this.props.result;
-        if (data.length === 0) {
-            return <></>
-        }
-
-        return (
-            <div className="result-list">
-                <ul>    
-                    {data.map((person, index) => (
-                        <li key={index} className="person">
-                            <h2>{person.name}</h2>
-                            <p>Height: {person.height}</p>
-                            <p>Mass: {person.mass}</p>
-                            <p>Hair Color: {person.hair_color}</p>
-                            <p>Skin Color: {person.skin_color}</p>
-                            <p>Eye Color: {person.eye_color}</p>
-                            <p>Birth Year: {person.birth_year}</p>
-                            <p>Gender: {person.gender}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+const List: React.FC <Props> = ({result}) => {
+    return (
+        result ?
+        <div className="result-list">
+            <ul>    
+                {result.map((person, index) => (
+                    <li key={index} className="person">
+                        <h2>{person.name}</h2>
+                        <p>Height: {person.height}</p>
+                        <p>Mass: {person.mass}</p>
+                        <p>Hair Color: {person.hair_color}</p>
+                        <p>Skin Color: {person.skin_color}</p>
+                        <p>Eye Color: {person.eye_color}</p>
+                        <p>Birth Year: {person.birth_year}</p>
+                        <p>Gender: {person.gender}</p>
+                    </li>
+                ))}
+            </ul>
+        </div> :
+        <></>
+    )
 }
 
 export default List;
