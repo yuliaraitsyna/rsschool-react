d763444f46ac8eba56ca491756042a3dc91650c5
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-07-10 16:12:00 +0100

    refactor: refactor App component to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -7,65 +7,49 @@ import ErrorButton from './error_handling/ErrorButton';
 
 interface Props {}
 
-interface State {
-  result: Person[],
-  loading: boolean
-}
+const App: React.FC<Props> = () => {
+  const [result, setResult] = React.useState<Person[]>([]);
+  const [loading, setLoading] = React.useState(true);
 
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      result: [],
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchInitialData();
-  }
-
-  fetchInitialData = () => {
-    this.setState({ loading: true });
+  React.useEffect(() => {
+    setLoading(true);
     fetch(`https://swapi.dev/api/people/`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        return response.json();
-      })
-      .then(data => {
-        setTimeout(() => {
-          this.setState({ result: data.results, loading: false });
-        }, 1000)
-      })
-      .catch(error => {
-        console.error("Error fetching data: ", error);
-        this.setState({ loading: false });
-      });
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          return response.json();
+        })
+        .then(data => {
+          setTimeout(() => {
+            setResult(data.results);
+            setLoading(false);
+          }, 1000)
+        })
+        .catch(error => {
+          console.error("Error fetching data: ", error);
+          setLoading(false);
+        });
+  }, []);
+
+  const handleSearchResults = (result: Person[]) => {
+    setResult(result);
   };
 
-  handleSearchResults = (result: Person[]) => {
-    this.setState({ result });
+  const handleLoadingState = (loading: boolean) => {
+    setLoading(loading);
   };
 
-  handleLoadingState = (loading: boolean) => {
-    this.setState({ loading });
-  };
-
-  render() {
-    const { result, loading } = this.state;
-    return (
-      <div className='app'>
-        <div className='upper-component'>
-          <ErrorButton></ErrorButton>
-          <h1>Star Wars search</h1>
-          <Search onSearchResult={this.handleSearchResults} setLoading={this.handleLoadingState}></Search>
-        </div>
-        {loading ? <div>Loading...</div> : <List result={result}></List>}
+  return (
+    <div className='app'>
+      <div className='upper-component'>
+        <ErrorButton></ErrorButton>
+        <h1>Star Wars search</h1>
+        <Search onSearchResult={handleSearchResults} setLoading={handleLoadingState}></Search>
       </div>
-    );
-  }
+      {loading ? <div>Loading...</div> : <List result={result}></List>}
+    </div>
+  );
 }
 
 export default App;
