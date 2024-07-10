2cf12b38e6c2c70d5d00e05fb27c79dbcc4dd1a9
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-07-10 16:47:13 +0100

    fix: fix unmount in hook

--- src/models/useLocalStorage.ts
+++ src/models/useLocalStorage.ts
@@ -1,25 +1,23 @@
 import React from "react";
 
-function useLocalStorage<T>(key: string, defaultValue: T): [T, (value: T) => void] {
-    const [value, setValue] = React.useState<T>(() => {
-        try {
-            const storedValue = localStorage.getItem(key);
-            return storedValue ? JSON.parse(storedValue) : defaultValue;
-        }
-        catch(error) {
-            console.error(`Error parsing localStorage key "${key}":`, error);
-            return defaultValue;
-        }
+function useLocalStorage(key: string, defaultValue: string): [string, (value: string) => void] {
+    const [value, setValue] = React.useState<string>(() => {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : defaultValue;
     });
 
     React.useEffect(() => {
         try {
             localStorage.setItem(key, JSON.stringify(value));
-        }
-        catch(error) {
+        } catch (error) {
             console.error(`Error setting localStorage key "${key}":`, error);
         }
+    }, [key, value]);
 
+    React.useEffect(() => {
+        return () => {
+            localStorage.setItem(key, JSON.stringify(value));
+        };
     }, [key, value]);
 
     return [value, setValue];
