14693770e28821e44c6de7042af7f4b00193dbe2
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-07-13 11:32:59 +0100

    feat: add tests for List component

---
+++ src/mockPeople.ts
@@ -0,0 +1,25 @@
+export const mockPeople = [
+    {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      hair_color: 'blond',
+      skin_color: 'fair',
+      eye_color: 'blue',
+      birth_year: '19BBY',
+      gender: 'male',
+      url: 'https://swapi.dev/api/people/1/'
+    },
+    {
+      name: 'Darth Vader',
+      height: '202',
+      mass: '136',
+      hair_color: 'none',
+      skin_color: 'white',
+      eye_color: 'yellow',
+      birth_year: '41.9BBY',
+      gender: 'male',
+      url: 'https://swapi.dev/api/people/4/'
+    }
+  ];
+  
---
+++ src/test/List.test.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom"
+import List from "../components/list/List"
+import { render, screen } from "@testing-library/react";
+import { mockPeople } from "../mockPeople";
+
+test('Renders specified number of cards', () => {
+    render(
+        <BrowserRouter>
+            <List result={mockPeople} currentPage={1} totalPages={9} onPageChange={() => {}} onItemClick={() => {}}></List>
+        </BrowserRouter>
+    );
+
+    const items = screen.getAllByRole('listitem');
+    expect(items).toHaveLength(mockPeople.length);
+});
+
+test('An appropriate message is displayed if no cards are present', () => {
+    render(
+        <BrowserRouter>
+            <List result={[]} currentPage={0} totalPages={0} onPageChange={() => {}} onItemClick={() => {}}></List>
+        </BrowserRouter>
+    )
+
+    expect(screen.getByText("No results found.")).toBeInTheDocument();
+})
