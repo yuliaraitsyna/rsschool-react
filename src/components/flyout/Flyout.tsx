f45e74c71b872e42c6689955614a3f645c334454
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-07-19 17:39:36 +0300

    feat: add downloading csv file

--- src/components/flyout/Flyout.tsx
+++ src/components/flyout/Flyout.tsx
@@ -3,6 +3,7 @@ import { RootState } from "../../redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { unselectAll } from "../../redux/cardsSlice";
 import "./Flyout.css"
+import downloadCSV from "../../models/downloadCSV";
 
 const Flyout: React.FC = () => {
     const selectedCards = useSelector((state: RootState) => state.cards.selectedCards);
@@ -11,11 +12,16 @@ const Flyout: React.FC = () => {
     const handleUnselectAll = () => {
         dispatch(unselectAll());
     }
+
+    const hadnleDownload = () => {
+        console.log(selectedCards)
+        downloadCSV(selectedCards);
+    }
     
     return (
         selectedCards.length > 0 ?
         <div className="flyout">
-            <button className="download-btn">{`Download ${selectedCards.length} items`}</button>
+            <button className="download-btn"  onClick={hadnleDownload}>{`Download ${selectedCards.length} items`}</button>
             <button className="unselect-all-btn" onClick={handleUnselectAll}>Unselect all</button>
         </div>
         :
